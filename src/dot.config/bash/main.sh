4ad461f024388e7caa37a649f31fb720e9801e3c
kumarstack55 <kumarstack55@gmail.com>
2024-09-28 16:33:45 +0900

    refactor: main.sh を更新した

--- src/dot.config/bash/main.sh
+++ src/dot.config/bash/main.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+_dotfiles__editor_list=("vim" "vi")
+
 dotfiles::test_command_exists() {
   local command="$1"
   type "${command}" 2>&1 >/dev/null
 }
 
-if dotfiles::test_command_exists vim; then
-  export EDITOR=vim
-elif dotfiles::test_command_exists vi; then
-  export EDITOR=vi
-fi
+dotfiles::configure_editor() {
+  local editor
+
+  for editor in ${_dotfiles__editor_list[@]}; do
+    if dotfiles::test_command_exists "${editor}"; then
+      export EDITOR="${editor}"
+      break
+    fi
+  done
+}
+
+dotfiles::configure_editor
